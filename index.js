882f0bb3314dfeb8d455f3038b46b4788135bc26
“saschakala” <mxsaschakala@gmail.com>
2020-11-17 20:41:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"]
+const event = "surprise"
+
+function writeCards(names, event) {
+    let birthday =[];
+    for (let i= 0; i < names.length; i++) {
+    birthday[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return birthday;
+  }
+
+  function countDown(number) {
+        while (number > -1) {
+            console.log(number--)
+        }
+  }
